0663019c61cdccf048874e79cb7d641786717261
LorenzoA98 <56697382+LorenzoA98@users.noreply.github.com>
2022-03-24 17:40:51 +0100

    ops upgrade

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ client.on('message', async (message) => {
             return message.channel.send('Entra in un canale vocale e riscrivi il comando ops.')
         }else{
             
-            message.member.voice.setChannel(874259489606492228);
-            message.member.voice.setChannel(874259489606492229);
-            message.member.voice.setChannel(898248263797440564);
-            message.member.voice.setChannel(880582204877590528);
-            message.member.voice.setChannel(902847813967306782);
-            message.member.voice.setChannel(938382556477411349);
+            message.author.voice.setChannel(874259489606492228);
+            message.author.voice.setChannel(874259489606492229);
+            message.author.voice.setChannel(898248263797440564);
+            message.author.voice.setChannel(880582204877590528);
+            message.author.voice.setChannel(902847813967306782);
+            message.author.voice.setChannel(938382556477411349);
             
             return message.channel.send('ops.');
         }
